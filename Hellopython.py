b1f521e6438bfce63f676cefed7c2e14c6b6f70e
Asif Ekhlaque <132199879+Asifekhlaque@users.noreply.github.com>
2023-09-12 17:51:55 +0530

    Add files via upload

---
+++ Hellopython.py
@@ -0,0 +1,3 @@
+#First Python code
+print("Hello Python")# Simple Print command
+#Python journy Begin
---
+++ Twodecimalnumber.py
@@ -0,0 +1,7 @@
+weight=2
+kg=weight/1000
+print(kg,"kg")
+round(weight,2)
+print(kg,"kg")
+print("%.2f"%kg,"kg")
+print("{:.2f}".format(kg),"kg")
